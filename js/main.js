1cbdf010647dd9a0e8e0f35471426d4cbe17a8b9
Michel Neumann <neumann.michel93@gmail.com>
2017-12-06 11:36:39 +0100

    add step 4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 import { Group } from './cloudless.js';
+
+const group = new Group({
+    "name": "livecoding",
+    "identifier": "com.spreewunder.cloudless",
+    "signaling": {
+        "servers": [{
+            "urls": "stun:stun.l.google.com:19302"
+        }],
+
+        "policies": {
+            "spreewunder.com": {
+                "type": "centralized",
+                "settings": {
+                    "address": "cloudless.spreewunder.com",
+                    "port": 3003,
+                    "secure": true
+                }
+            }
+        }
+    }
+});
